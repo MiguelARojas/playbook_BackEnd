65fa59e382ae4ab3e8177192017c89c4c67cc29d
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 18:23:32 -0500

    Se agrego comentarios para enteneder mejor los ejercicios

--- weekly_mission_1/main.js
+++ weekly_mission_1/main.js
@@ -59,15 +59,17 @@ console.log(myPhone)
 
 // Declarando un objeto con variables locales y publicas
 const myCase = (() => {
-    // Variables de contexto local
+    // Variables de contexto local, no podemos darle uso a menos que las llamemos en const exported
     const colorCase = "Red with Black"
     const typeConnect = 'Type C'
     const charger = "Fast Charge"
+
 
     // Variable para guardar las variables locales
     const exported = {
         colorPhone: "The cellphone is white",
         chargerConnection: "The type of the charger is type C",
+            // de esta manera asignamos las variables locales y podemos darles uso
             publicCharger: charger,
             publicColor: colorCase,
             publicConnect: typeConnect
@@ -77,3 +79,4 @@ const myCase = (() => {
 })()
 
 console.log(myCase)
+
