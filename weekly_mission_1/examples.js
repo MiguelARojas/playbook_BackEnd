ae5d6331683f533edac9ead87210c39cb5d3c6a6
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 18:46:47 -0500

    Realizando ejemplos de como hacer llamado informacion de un archivo a otro

---
+++ weekly_mission_1/examples.js
@@ -0,0 +1,9 @@
+
+// con el exports podemos traer la informacion de otro archivo y lo almacenamos en message
+exports.info = (message) => {
+    console.log(`The message to info is: ${message}`)
+}
+
+exports.delete = (message) => {
+    console.log(`The other message is: ${message}`)
+}
--- weekly_mission_1/exercise_2.js
+++ weekly_mission_1/exercise_2.js
@@ -1,5 +1,7 @@
 // aqui estamos creando una variable donde asignamos que necesitamos otro archivo
-const example = require('./examples.js')
+const example = require('./examples')
 
 example.info('This is a message you only can see if you have the file "examples.js"')
+
+// si no llamamos el example.delete en el otro archivo nos generara error
 example.delete('This is another message')
