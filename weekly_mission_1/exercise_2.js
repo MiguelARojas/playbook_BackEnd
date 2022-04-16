99f7053798e6da691671be7e743e2de491084b30
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 20:05:18 -0500

    Organizando estructura de los archivos require

--- weekly_mission_1/exercise_2.js
+++ weekly_mission_1/exercise_2.js
@@ -1,5 +1,5 @@
 // aqui estamos creando una variable donde asignamos que necesitamos otro archivo
-const example = require('./examples')
+const example = require('./requerie/examples')
 
 example.info('This is a message you only can see if you have the file "examples.js"')
 
