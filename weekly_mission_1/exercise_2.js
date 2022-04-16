2670f24d05a5caf3d59cf9474a75587b6a173a06
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 20:13:15 -0500

    Creando el ejercicio 3 para comprender como funcionan los require

--- weekly_mission_1/exercise_2.js
+++ weekly_mission_1/exercise_2.js
@@ -1,5 +1,5 @@
 // aqui estamos creando una variable donde asignamos que necesitamos otro archivo
-const example = require('./requerie/examples')
+const example = require('./require/example_1')
 
 example.info('This is a message you only can see if you have the file "examples.js"')
 
---
+++ weekly_mission_1/exercise_3.js
@@ -0,0 +1,9 @@
+// main.js
+
+// en este ejercicio veremos que podemos tener mas de dos requerie y poder llamar informacion de dos diferentes archivos
+
+const logger1 = require('./require/example_2')
+const logger2 = require('./require/example_3')
+
+logger1('This the first informational message')
+logger2.lastMessage('This is the second informational message')
