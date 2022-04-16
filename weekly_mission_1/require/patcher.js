e26daf1b732002f8df48a3a089a5998c80d3f7ff
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 20:47:29 -0500

    Creacion del archivo patcher

---
+++ weekly_mission_1/require/patcher.js
@@ -0,0 +1,13 @@
+/*
+    require('./logger') 
+    Nos dara el objeto creado
+    
+    require('./logger').Logger 
+    Nos dara la clase
+*/
+
+// aqui no es necesario llamar la carpeta require porque se encuentra donde mismo este archivo
+require('./example_6').customMessage = function () {
+    console.log("This is a new funcionality")
+}
+
