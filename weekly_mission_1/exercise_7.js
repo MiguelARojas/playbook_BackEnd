3b6c85b73cfe6c6d9e5c34357a0db75f61bf0fc0
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 21:04:55 -0500

    Creacion de los mensajes de nuestros pokemons

--- weekly_mission_1/exercise_7.js
+++ weekly_mission_1/exercise_7.js
@@ -1,2 +1,15 @@
 // llamamos nuestro archivo donde tiene nuestros metodos
 const Pokemon = require('./require/pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+
+pikachu.sayHello()
+pikachu.sayMessage("Hello World")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("I'm alive")
+
+squirtle.sayHello()
+squirtle.sayMessage("This is my last message")
