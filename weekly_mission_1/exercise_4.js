da8c2fe2a538ab4a0e49ac5c240fd6595abee677
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-15 20:21:42 -0500

    Creacion del 4 ejercicio con sus respectivo require

---
+++ weekly_mission_1/exercise_4.js
@@ -0,0 +1,13 @@
+// invocamos el modulo que contendra nuestra clase
+const Logger = require('./require/example_4')
+
+// creamos nuestro objeto el cual llama al constructor de nuestra clase
+const DbLogger = new Logger('DB')
+
+// invocamos al metodo
+DbLogger.info('This is a message')
+
+
+// creamos otro objeto
+const access = new Logger('ACCESS')
+access.info2('This is another message')
