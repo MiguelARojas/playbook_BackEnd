513a76d06b7bb0797478e52feb399ff040cf7ff4
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 16:45:59 -0500

    Ejemplo 3 - Objeto con atributos y haciendo correcciones en ejemplos pasados

--- weekly_mission_2/example_2.js
+++ weekly_mission_2/example_2.js
@@ -9,5 +9,23 @@ class game {
 }
 const myGame = new game() // podemos crear varios objetos de la clase game
 console.log("\nEjemplo 2 - Creando un objeto a partir de una clase")
-console.log("My class is: " + game)
-console.log("My object is: " + myGame)
+console.log("My class is: ")
+console.log(game)
+console.log("My object is: ")
+console.log(myGame)
+
+
+// Ejemplo 3 - Objeto con atributos
+class person {
+    constructor(name,age,weight,height){
+        this.name = name
+        this.age = age
+        this.weight = weight
+        this.height = height
+    }
+}
+
+// crearemos un objeto a partir de la clase
+const Miguel = new person("Miguel",22,94,1.87)
+console.log("\nNuestro objeto con atributos a partir de una clase")
+console.log(Miguel)
