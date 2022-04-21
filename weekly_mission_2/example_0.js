70cc2b0c456f40aa39195aeefdcb514975f937cf
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 11:57:34 -0500

    Ejemplo_2 - Creando un objeto con propiedades y imprimiendo el objeto

--- weekly_mission_2/example_0.js
+++ weekly_mission_2/example_0.js
@@ -5,4 +5,25 @@ const myFirstObject = {} // de esta manera se crea un objeto vacio
 console.log("Ejemplo 1. Creando un Objeto Vació")
 
 // imprimimos nuestro objeto
-console.log(myFirstObject)
+console.log(myFirstObject) // nos debe imprimir {}
+
+
+// Ejemplo 2 - Crear un objeto con propiedadesp o valores
+const mySecondObject = {
+    name: "Miguel",
+    lastName: "Rojas",
+    age: "22",
+    city: "Guadalajara",
+    school: "CUCEI",
+    pets: "1 Hamster, 2 Dogs"
+}
+
+console.log("Ejemplo 2. Creando un objeto con propiedades o valores")
+
+// podemos imprimir el objeto directamente
+console.log(mySecondObject)
+
+// o podemos imprimir cierta informacion del objeto
+console.log(mySecondObject.name)
+console.log(mySecondObject.lastName)
+console.log(mySecondObject.pets)
