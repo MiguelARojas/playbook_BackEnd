f1b8c3b142f8cfaa38515a2740d5ae6cd12a6b16
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 12:00:33 -0500

    Mejorando visualizacion de la consola (saltos de linea)

--- weekly_mission_2/example_0.js
+++ weekly_mission_2/example_0.js
@@ -1,6 +1,5 @@
-console.log("Semana 2 - Example 0")
-
-// Ejemplo 1 - Como crear un objeto en Js
+console.log("Semana 2 - Example 0\n")
+    // Ejemplo 1 - Como crear un objeto en Js
 const myFirstObject = {} // de esta manera se crea un objeto vacio
 console.log("Ejemplo 1. Creando un Objeto Vació")
 
@@ -18,12 +17,20 @@ const mySecondObject = {
     pets: "1 Hamster, 2 Dogs"
 }
 
+// agregamos un salto de linea para que se vizualice mejor la consola
+console.log("\n")
+
 console.log("Ejemplo 2. Creando un objeto con propiedades o valores")
 
 // podemos imprimir el objeto directamente
+console.log("Imprimiendo mi objeto")
 console.log(mySecondObject)
 
+// agregamos un salto de linea para que se vizualice mejor la consola
+console.log("\n")
+
 // o podemos imprimir cierta informacion del objeto
+console.log("Imprimiendo valores de mi objeto")
 console.log(mySecondObject.name)
 console.log(mySecondObject.lastName)
 console.log(mySecondObject.pets)
