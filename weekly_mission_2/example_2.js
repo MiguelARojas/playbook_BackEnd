01c4fd278e1261b69bde44c229c082215ef9fc3d
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 16:40:56 -0500

    Ejemplo 2 - Creacion de un objeto a partir de una clase

--- weekly_mission_2/example_2.js
+++ weekly_mission_2/example_2.js
@@ -1,6 +1,13 @@
 // Ejemplo 1 - Clase Vacía
 class animal {
-
 }
-console.log("Ejemplo 1 - Creando una clase vacía")
+console.log("\nEjemplo 1 - Creando una clase vacía")
 console.log(animal)
+
+// Ejemplo 2 - Objeto a partir de una clase
+class game {
+}
+const myGame = new game() // podemos crear varios objetos de la clase game
+console.log("\nEjemplo 2 - Creando un objeto a partir de una clase")
+console.log("My class is: " + game)
+console.log("My object is: " + myGame)
