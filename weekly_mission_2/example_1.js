ba50269099b2a45f890e5989f711d1d4478e4909
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 12:53:43 -0500

    Ejemplo 2 - For Each para suma de mi arreglo

--- weekly_mission_2/example_1.js
+++ weekly_mission_2/example_1.js
@@ -2,10 +2,16 @@
 const numbers = [1,2,3,4,5];
 const completeName = ["Miguel","Angel","Rojas","Macias"];
 
-console.log("Ejemplo 1.- For Each")
+console.log("\nEjemplo 1.- For Each")
 
 console.log("Imprimimos el arreglo numbers")
 numbers.forEach(num => console.log(num))
 
-console.log("Imprimimos el arreglo completeName")
+console.log("\nImprimimos el arreglo completeName")
 completeName.forEach(num => console.log(num))
+
+// Ejemplo 2 - For Each, Calcular la suma de un arreglo
+let sum = 0;
+const numbers2 = [1,2,3,4,5];
+numbers2.forEach(num => sum += num)
+console.log(`\nLa suma de tu arreglo es: ${sum}`)
