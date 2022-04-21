49cb72e4519a7e8619d663ff6723934bada83235
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 12:50:45 -0500

    Ejemplo 1 - For Each para arreglos

---
+++ weekly_mission_2/example_1.js
@@ -0,0 +1,11 @@
+// Ejemplo 1 - for each para imprimir los elementos de una lista
+const numbers = [1,2,3,4,5];
+const completeName = ["Miguel","Angel","Rojas","Macias"];
+
+console.log("Ejemplo 1.- For Each")
+
+console.log("Imprimimos el arreglo numbers")
+numbers.forEach(num => console.log(num))
+
+console.log("Imprimimos el arreglo completeName")
+completeName.forEach(num => console.log(num))
