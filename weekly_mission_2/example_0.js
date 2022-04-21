b883c346e851cd80f334e3b1049bf89db4aaba66
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-21 11:53:30 -0500

    Ejemplo_1 - Creando un objeto vacio en Js

---
+++ weekly_mission_2/example_0.js
@@ -0,0 +1,8 @@
+console.log("Semana 2 - Example 0")
+
+// Ejemplo 1 - Como crear un objeto en Js
+const myFirstObject = {} // de esta manera se crea un objeto vacio
+console.log("Ejemplo 1. Creando un Objeto Vació")
+
+// imprimimos nuestro objeto
+console.log(myFirstObject)
