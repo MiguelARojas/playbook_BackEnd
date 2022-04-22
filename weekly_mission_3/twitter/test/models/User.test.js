4cf6f20fbb40d724f83d4359083dfaa7f4acbbab
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-22 14:08:10 -0500

    Modificacion de rutas y realizacion de prueba unitaria de mi clase User

---
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -0,0 +1,18 @@
+// importamos nuestra clase user
+const User = require('../../app/user')
+
+describe('Unit Tests for User Class', () => {
+    test('1. Create an User Object', () => {
+        // creamos un objeto de nuestra clase User
+        const newUser = new User(1,"Mikael","Miguel","Bio","dateCreated","lastUpdated")
+
+        // generamos las pruebas
+        expect(newUser.id).toBe(1)
+        expect(newUser.username).toBe("Mikael")
+        expect(newUser.name).toBe("Miguel")
+        expect(newUser.bio).toBe("Bio")
+        expect(newUser.dateCreated).toBe("dateCreated")
+        expect(newUser.lastUpdated).toBe("lastUpdated")
+
+    });
+})
