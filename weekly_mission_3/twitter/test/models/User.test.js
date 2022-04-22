e9a83f9f1a8203f34904f7412ede2b6d91c907a5
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-22 14:09:58 -0500

    Modificacion en nuestra prueba de unidad

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -4,15 +4,15 @@ const User = require('../../app/user')
 describe('Unit Tests for User Class', () => {
     test('1. Create an User Object', () => {
         // creamos un objeto de nuestra clase User
-        const newUser = new User(1,"Mikael","Miguel","Bio","dateCreated","lastUpdated")
+        const newUser = new User(1,"Mikael","Miguel","Bio")
 
         // generamos las pruebas
         expect(newUser.id).toBe(1)
         expect(newUser.username).toBe("Mikael")
         expect(newUser.name).toBe("Miguel")
         expect(newUser.bio).toBe("Bio")
-        expect(newUser.dateCreated).toBe("dateCreated")
-        expect(newUser.lastUpdated).toBe("lastUpdated")
+        expect(newUser.dateCreated).not.toBeUndefined()
+        expect(newUser.lastUpdated).not.toBeUndefined()
 
     });
 })
