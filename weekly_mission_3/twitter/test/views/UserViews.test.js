2d7956f36d30ebc449e7eee8e40ee5be2f8bb0cc
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-22 19:07:12 -0500

    Realizacion del ultimo requerimiento

--- weekly_mission_3/twitter/test/views/UserViews.test.js
+++ weekly_mission_3/twitter/test/views/UserViews.test.js
@@ -19,5 +19,21 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 
-    test("Return an error object when try to create a new user with a payload with missing properties")
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+
+        // realizamos nuestra validacion
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
+
+    test("Creat a user by given valid payload", () => {
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload)
+
+        // realizamos nuestras validaciones
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    })
 })
