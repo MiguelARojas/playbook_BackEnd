5053170220eb4e015f9c80160ab4266d376a1d45
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-22 20:14:42 -0500

    Realizando conexion con postman

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -16,8 +16,8 @@ app.get('/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     // creamos nuestros objetos
     const explorer1 = {id:1, name:"Valente"}
-    const explorer2 = {id:1, name:"Miguel"}
-    const explorer3 = {id:1, name:"Victor"}
+    const explorer2 = {id:2, name:"Miguel"}
+    const explorer3 = {id:3, name:"Victor"}
 
     // asignamos nuestros objetos a una lista de objeto
     const explorers = [explorer1,explorer2,explorer3]
